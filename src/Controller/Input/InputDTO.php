be8f599928ff87f2f79e9da2c8130271b92b7bd3
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-01-24 21:11:43 +0300

    Add enum validation

--- src/Controller/Input/InputDTO.php
+++ src/Controller/Input/InputDTO.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller\Input;
 
+use App\Enums\ValueEnum;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class InputDTO
 {
-    #[Assert\Choice(choices: ['correct', 'valid'], multiple: true)]
+    #[Assert\Choice(callback: [ValueEnum::class, 'cases'], multiple: true)]
     #[Assert\Type('array')]
     public array $choices;
 
     public static function fillFromRequest(Request $request): self {
         $result = new self();
-        $result->choices = $request->request->all('choices');
+        $result->choices = array_map(
+            static fn(string $choice): ?ValueEnum => ValueEnum::tryFrom($choice),
+            $request->request->all('choices')
+        );
 
         return $result;
     }
---
+++ src/Enums/ValueEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum ValueEnum: string
+{
+    case Valid = 'valid';
+    case Correct = 'correct';
+}
